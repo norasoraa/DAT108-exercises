3c8c5fc7e3d80e40c4202fa1b405a16a47a5f58e
Nora Soraa <notjsoraa@icloud.com>
2024-08-22 09:57:27 +0200

    Initial commit

---
+++ src/task4/ConditionalPrint.java
@@ -0,0 +1,12 @@
+package task4;
+
+public class ConditionalPrint {
+  
+  public static void main(String[] args) {
+    
+  }
+  
+  private static void conditionalPrint(String s, boolean b) {
+    
+  }
+}
